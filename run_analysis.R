018ea7eb4169940f08e09a3df7998194bbd8b4c1
DIEGO CARDONE <dcardone@gmail.com>
2025-12-10 13:07:20 -0300

    There was a mistake using the parameter sep=" "  in read.csv to get X_train which caused the dataframe to be incorrect. this was corrected.

--- run_analysis.R
+++ run_analysis.R
@@ -3,24 +3,18 @@ directory_path <-"./getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset"
 # Read txt file check that the separator of columns is a blank space
 X_train <- read.csv(paste0(directory_path, "/train/X_train.txt"), sep = " ", header = FALSE)
 
-#check for the number of columns and rows:
-# according to the Readme.txt from the dataset each row should have: 
-# 3 accelerations X, Y, Z
-# 3 estimated body accelerations
-# 3 velocities from the gyroscope
-# 561 features columns with time and frequency domain variables
-# 1 activity label
-# 1 subject id.
-# total = 662
-
 dim(X_train)
 
 #take a look to the dataframe
 head(X_train)
 
+X_train[78,]
+#there are some NA values in the data frame that need to be investigated
+
+sum(is.na(X_train[5,]))
+#it looks that every row has the same quantity of NAs (101 so 662 - 101 = 561) it 
+
 #Get the 561 features names
-
-
 features <- read.csv(paste0(directory_path, "/features.txt"), sep = " ", header = FALSE)
 
 head(features)
